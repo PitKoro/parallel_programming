f43d0076c67a295c24a32221ba4bc9b3c2efd377
PitKoro <pit.koro@gmail.com>
2022-03-12 12:38:29 +0300

    edit

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 
 #define CSC(call) 														\
@@ -16,10 +15,10 @@
 
 
 // Поэлементно перемножает вектора
-__global__ void kernel(int* first_vector, int* second_vector, int* result_vector, int vector_size) {
+__global__ void kernel(double* first_vector, double* second_vector, double* result_vector, double vector_size) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int offset = blockDim.x * gridDim.x;
-    while(idx < vector_size) {
+    if (idx < (int)vector_size) {
         result_vector[idx] = first_vector[idx] * second_vector[idx];
         idx += offset;
     }
@@ -27,39 +26,39 @@ __global__ void kernel(int* first_vector, int* second_vector, int* result_vector
 
 
 int main() {
-    int vector_size;
-    scanf("%d", &vector_size);
+    double vector_size;
+    scanf("%lf", &vector_size);
 
-    int first_vector[vector_size];
-    int second_vector[vector_size];
+    double first_vector[(int)vector_size];
+    double second_vector[(int)vector_size];
 
-    for (int i = 0; i < vector_size; i++) {
-        scanf("%d", &first_vector[i]);
+    for (int i = 0; i < (int)vector_size; i++) {
+        scanf("%lf", &first_vector[i]);
     }
 
-    for (int i = 0; i < vector_size; i++) {
-        scanf("%d", &second_vector[i]);
+    for (int i = 0; i < (int)vector_size; i++) {
+        scanf("%lf", &second_vector[i]);
     }
 
 
-    int *result = new int[vector_size];
-    int *device_first_vector = new int[vector_size];
-    int *device_second_vector = new int[vector_size];
-    int *device_result_vector = new int[vector_size];
+    double *result = new double[(int)vector_size];
+    double *device_first_vector = new double[(int)vector_size];
+    double *device_second_vector = new double[(int)vector_size];
+    double *device_result_vector = new double[(int)vector_size];
 
-    CSC(cudaMalloc(&device_first_vector, sizeof(int) * vector_size));
-    CSC(cudaMalloc(&device_second_vector, sizeof(int) * vector_size));
-    CSC(cudaMalloc(&device_result_vector, sizeof(int) * vector_size));
+    CSC(cudaMalloc(&device_first_vector, sizeof(double) * (int)vector_size));
+    CSC(cudaMalloc(&device_second_vector, sizeof(double) * (int)vector_size));
+    CSC(cudaMalloc(&device_result_vector, sizeof(double) * (int)vector_size));
 
-    CSC(cudaMemcpy(device_first_vector, &first_vector, sizeof(int) * vector_size, cudaMemcpyHostToDevice));
-    CSC(cudaMemcpy(device_second_vector, &second_vector, sizeof(int) * vector_size, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpy(device_first_vector, &first_vector, sizeof(double) * (int)vector_size, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpy(device_second_vector, &second_vector, sizeof(double) * (int)vector_size, cudaMemcpyHostToDevice));
 
-    kernel<<<256, 256>>>(device_first_vector, device_second_vector, device_result_vector, vector_size);
+    kernel<<<256, 256>>>(device_first_vector, device_second_vector, device_result_vector, (int)vector_size);
 
     CSC(cudaDeviceSynchronize());
     CSC(cudaGetLastError());
 
-    CSC(cudaMemcpy(result, device_result_vector, sizeof(int) * vector_size, cudaMemcpyDeviceToHost));
+    CSC(cudaMemcpy(result, device_result_vector, sizeof(double) * (int)vector_size, cudaMemcpyDeviceToHost));
 
     CSC(cudaFree(device_first_vector));
     CSC(cudaFree(device_second_vector));
@@ -67,11 +66,11 @@ int main() {
 
 
     // Вывод в stdout
-    for (int i = 0; i < vector_size; i++) {
-        if (i != vector_size - 1) {
-            fprintf(stdout, "%.10e ", double(result[i]));
+    for (int i = 0; i < (int)vector_size; i++) {
+        if (i != (int)vector_size - 1) {
+            fprintf(stdout, "%.10e ", result[i]);
         } else {
-            fprintf(stdout, "%.10e\n", double(result[i]));
+            fprintf(stdout, "%.10e", result[i]);
         }
         
     }
