768ab2e91a390522af9423e04b4960c1d8af8669
PitKoro <pit.koro@gmail.com>
2022-03-12 23:04:47 +0300

    checker OK

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -18,7 +18,7 @@
 __global__ void kernel(double* first_vector, double* second_vector, double* result_vector, double vector_size) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int offset = blockDim.x * gridDim.x;
-    if (idx < (int)vector_size) {
+    while (idx < (int)vector_size) {
         result_vector[idx] = first_vector[idx] * second_vector[idx];
         idx += offset;
     }
@@ -29,8 +29,8 @@ int main() {
     double vector_size;
     scanf("%lf", &vector_size);
 
-    double first_vector[(int)vector_size];
-    double second_vector[(int)vector_size];
+    double *first_vector = new double[(int)vector_size];
+    double *second_vector = new double[(int)vector_size];
 
     for (int i = 0; i < (int)vector_size; i++) {
         scanf("%lf", &first_vector[i]);
@@ -50,8 +50,8 @@ int main() {
     CSC(cudaMalloc(&device_second_vector, sizeof(double) * (int)vector_size));
     CSC(cudaMalloc(&device_result_vector, sizeof(double) * (int)vector_size));
 
-    CSC(cudaMemcpy(device_first_vector, &first_vector, sizeof(double) * (int)vector_size, cudaMemcpyHostToDevice));
-    CSC(cudaMemcpy(device_second_vector, &second_vector, sizeof(double) * (int)vector_size, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpy(device_first_vector, first_vector, sizeof(double) * (int)vector_size, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpy(device_second_vector, second_vector, sizeof(double) * (int)vector_size, cudaMemcpyHostToDevice));
 
     kernel<<<256, 256>>>(device_first_vector, device_second_vector, device_result_vector, (int)vector_size);
 
@@ -63,9 +63,9 @@ int main() {
     // Вывод в stdout
     for (int i = 0; i < (int)vector_size; i++) {
         if (i != (int)vector_size - 1) {
-            fprintf(stdout, "%.10e ", result[i]);
+            fprintf(stdout, "%e ", result[i]);
         } else {
-            fprintf(stdout, "%.10e", result[i]);
+            fprintf(stdout, "%e", result[i]);
         }
         
     }
